2218de78861c1250320f4a42faa02af1cfba43f3
Petr Slajchrt <slajchrtp@avast.com>
2017-07-31 11:33:37 +0200

    fix default import err

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+import * as UIkit from 'uikit';
+import * as Icons from 'uikit/dist/js/uikit-icons';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
